4c5d78152b84024db56fabc71e0229ed45d95520
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-04-24 19:49:29 +0530

    Time: 5 ms (28.30%), Space: 7.7 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& v) {
+        priority_queue<int> pq(begin(v),end(v));
+        while(pq.size()>1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            if(x>y){
+                pq.push(x-y);
+            }
+            
+        }
+        return pq.empty()?0:pq.top();
+        
+    }
+};
