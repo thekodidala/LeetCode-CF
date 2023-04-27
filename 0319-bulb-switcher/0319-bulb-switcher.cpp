b2851ac324e506923f31aad0169c2244ad726298
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-04-27 22:49:50 +0530

    Time: 3 ms (45.34%), Space: 6.1 MB (9.91%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int bulbSwitch(int n) {
         int res=0,i=1;
+        //Here sqrt of n bulbs will glow finally..
         while(i*i<=n){
             i++;res++;
         }
