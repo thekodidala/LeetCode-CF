ccb04f690a5eefbdce275f6e6308b7b27f091774
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-05-04 20:08:16 +0530

    Time: 9 ms (48.47%), Space: 8.1 MB (37.02%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -4,11 +4,14 @@ public:
         queue<int>r,d;
         int n=senate.length();
         for(int i=0;i<n;i++){
+            //push the indices of R and D into their respective queue ds
             (senate[i]=='R')?r.push(i):d.push(i);
         }
         while(!r.empty()&&!d.empty()){
             int st1=r.front(),st2=d.front();
+            //removing both parties
             r.pop(),d.pop();
+            //it is a round table so we are adding at last again
             st1<st2?r.push(st1+n):d.push(st2+n);
         }
         return r.size()>d.size()?"Radiant":"Dire";
