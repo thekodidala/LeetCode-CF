ec5f2c8f02577f87d41067f6893d9c05398605e1
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-05-16 20:02:05 +0530

    Time: 1158 ms (54.44%), Space: 0B (100.00%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
 select tweet_id from Tweets 
-where length(content) >15;
+where char_length(content) >15;
+#char_length() gives length of string in character
+#where as length() gives length of string in bytes
