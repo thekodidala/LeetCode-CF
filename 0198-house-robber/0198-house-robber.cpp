a1b4a559c92967f3fe96b1c05824e9aed7eb0bdd
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-07-27 16:40:55 +0530

    Time: 3 ms (38.19%), Space: 7.6 MB (74.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,14 +3,15 @@ public:
     //interesting dp problem solved using tabulation (bottom up) approach
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1);
-        dp[0]=nums[0];
+        int prev=nums[0],prev2=0;
         for(int i=1;i<n;i++){
             int pick=nums[i];
-            if(i>1){pick+=dp[i-2];}
-            int notpick=0+dp[i-1];
-            dp[i]=max(pick,notpick);
+            if(i>1){pick+=prev2;}
+            int notpick=0+prev;
+            int curi=max(pick,notpick);
+            prev2=prev;
+            prev=curi;
         }
-        return dp[n-1];
+        return prev;
     }
 };
