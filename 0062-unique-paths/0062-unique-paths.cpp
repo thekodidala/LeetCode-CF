d815d65f3503b05fe1e5c2abdc5393fad4bebd69
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-07-28 11:59:26 +0530

    Time: 3 ms (38.26%), Space: 6 MB (85.50%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countpaths(int i,int j,vector<vector<int>>&dp){
+    /*int countpaths(int i,int j,vector<vector<int>>&dp){
         if(i==0&&j==0){return 1;}
         if(i<0||j<0){return 0;}
         if(dp[i][j]!=-1){return dp[i][j];}
@@ -8,9 +8,16 @@ public:
         left=countpaths(i,j-1,dp);
         up=countpaths(i-1,j,dp);
         return dp[i][j]=left+up;
-    }
+    }*/
+    //solved using combinations
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-       return countpaths(m-1,n-1,dp);
+        //there are total m-1 and n-1 moves to reach goal state
+        int N=m+n-2;
+        int r=m-1;
+        long long res=1;
+        for(int i=1;i<=r;i++){
+            res=res*(N-r+i)/i;
+        }
+        return res;
     }
 };
