ab1ecd2eca5349dc33dbaed18ba2a1f96d5a9358
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-08-01 17:17:14 +0530

    Time: 215 ms (83.00%), Space: 9.7 MB (90.49%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -5,21 +5,21 @@ public:
         for(int i=0;i<n;i++){totalsum+=nums[i];}
         if(totalsum%2)return false;
         int target=totalsum/2;
-        vector<vector<bool>>dp(n,vector<bool>(target+1,0));
-        
-        for(int i=0;i<n;i++)dp[i][0]=true;
-        if(nums[0]<=target)dp[0][nums[0]]=true;
+        vector<bool>prev(target+1,0),cur(target+1,0);
+        prev[0]=cur[0]=true;
+        if(nums[0]<=target)prev[nums[0]]=true;
         
         for(int ind=1;ind<n;ind++){
             for(int tar=1;tar<=target;tar++){
-                bool nottake=dp[ind-1][tar];
+                bool nottake=prev[tar];
                 bool take=false;
                 if(tar>=nums[ind]){
-                    take=dp[ind-1][tar-nums[ind]];
+                    take=prev[tar-nums[ind]];
                 }
-                dp[ind][tar]=nottake||take;
+                cur[tar]=nottake||take;
             }
+            prev=cur;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 };
