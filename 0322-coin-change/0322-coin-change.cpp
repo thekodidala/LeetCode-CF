8f61156be0548506d5687d1b3ce38c5a8e274034
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-08-11 22:52:00 +0530

    Time: 106 ms (60.17%), Space: 45.1 MB (38.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int possible(int ind,vector<vector<int>>&dp,vector<int>&coins,int amount){
+    /*int possible(int ind,vector<vector<int>>&dp,vector<int>&coins,int amount){
         if(ind==0){
             if(amount%coins[0]==0)return amount/coins[0];
             return 1e9;
@@ -13,12 +13,24 @@ public:
             pick=1+possible(ind,dp,coins,amount-coins[ind]);
         }
         return dp[ind][amount]=min(pick,notpick);
-    }
+    }*/
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=possible(n-1,dp,coins,amount);
-        if(ans>=1e9)return -1;
-        return ans;
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=i/coins[0];
+            else dp[0][i]=1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int a=0;a<=amount;a++){
+                int notpick=dp[i-1][a];
+                int pick=1e9;
+                if(coins[i]<=a){
+                    pick=1+dp[i][a-coins[i]];
+                }
+                dp[i][a]=min(pick,notpick);
+            }
+        }
+        return (dp[n-1][amount]>=1e9)?-1:dp[n-1][amount];
     }
 };
