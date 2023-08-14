df9804e9607298bc522f872fc936a2d0ce3018b2
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-08-14 22:14:42 +0530

    Time: 2 ms (39.56%), Space: 6.3 MB (14.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int ways(int n,vector<int>&dp){
-        if(n==0){return 1;}
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        int one=ways(n-1,dp);
-        int two=ways(n-2,dp);
-        return dp[n]=one+two;
-    }
+    // int ways(int n,vector<int>&dp){
+    //     if(n==0){return 1;}
+    //     if(n<0)return 0;
+    //     if(dp[n]!=-1)return dp[n];
+    //     int one=ways(n-1,dp);
+    //     int two=ways(n-2,dp);
+    //     return dp[n]=one+two;
+    // }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return ways(n,dp);
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        int one,two;
+        for(int i=1;i<=n;i++){
+            one=dp[i-1];
+            two=0;
+            if(i>1)
+                two=dp[i-2];
+            dp[i]=one+two;
+        }
+        return dp[n];
     }
 };
