f7299da990427ff67913c4afb5e59fe9d06f1a84
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-08-19 14:22:25 +0530

    Time: 7 ms (98.20%), Space: 20.5 MB (6.67%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -3,18 +3,12 @@ public:
     bool canPlaceFlowers(vector<int>& nums, int k) {
         int n=nums.size(),cnt=0;
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                bool left=(i==0)||(nums[i-1]==0);
-                bool right=(i==n-1)||(nums[i+1]==0);
-                if(left && right){
-                    nums[i]=1;
-                    cnt++;
-                }
-                if(cnt>=k){return true;}
+            if(nums[i]==0&&(i==0||nums[i-1]==0)&&(i==n-1||nums[i+1]==0)){
+                nums[i]=1;
+                cnt++;
             }
-            
+            if(cnt>=k){return true;}
         }
-        //cout<<cnt;
         return (cnt>=k);
     }
 };
