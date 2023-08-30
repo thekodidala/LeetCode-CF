1645165da7e643056ccd453d87fdbc4403d059df
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-08-30 21:11:02 +0530

    Time: 4 ms (58.47%), Space: 12.2 MB (23.58%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //implementation of next_permutation
-        int n=nums.size();
-        int ind=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind=i;
+        int n=nums.size(),a=-1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                a=i-1;
                 break;
             }
         }
-        if(ind!=-1){
-            for(int i=n-1;i>ind;i--){
-                if(nums[i]>nums[ind]){
-                    swap(nums[i],nums[ind]);
-                    break;
+            if(a!=-1){
+                for(int i=n-1;i>a;i--){
+                    if(nums[a]<nums[i]){
+                        swap(nums[a],nums[i]);
+                        break;
+                    }
                 }
+                sort(nums.begin()+a+1,nums.end());
             }
-            sort(nums.begin()+ind+1,nums.end());
-        }
-        else{
-            reverse(nums.begin(),nums.end());
-        }
+            else{
+                reverse(nums.begin(),nums.end());
+            }
         //return nums;
     }
 };
