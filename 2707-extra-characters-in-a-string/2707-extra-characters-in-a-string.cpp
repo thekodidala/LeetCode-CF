cc32938b44d666e5f282e5b904005ea4702b1731
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-09-02 23:55:47 +0530

    Time: 395 ms (22.01%), Space: 226.9 MB (8.46%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    map<string,int>mp;
-    int f(int ind,vector<int>&dp,string &s){
+    
+    int f(int ind,vector<int>&dp,string &s,unordered_map<string,int> &mp){
         int n=s.length();
         if(ind>=n)return 0;
         if(dp[ind]!=-1)return dp[ind];
-        int ans=INT_MAX;
-        ans=1+f(ind+1,dp,s);
+        int ans=n;
         string k="";
         for(int i=ind;i<n;i++){
             k+=s[i];
+            int cnt;
             if(mp[k]){
-                ans=min(ans,f(i+1,dp,s));
+                cnt=f(i+1,dp,s,mp);
             }
+            else{
+                cnt=k.size()+f(i+1,dp,s,mp);
+            }
+            ans=min(ans,cnt);
+            
         }
         return dp[ind]=ans;
     } 
     int minExtraChar(string s, vector<string>& dict) {
         int n=s.length();
+        unordered_map<string,int>mp;
         for(auto a:dict){mp[a]++;}
         vector<int>dp(n+1,-1);
-        int cnt=f(0,dp,s);
+        int cnt=f(0,dp,s,mp);
         return cnt;
     }
 };
