d6a3c83055d5a1affbcbdc3f03d572aa1692ec97
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-09-09 15:58:52 +0530

    Time: 4 ms (43.13%), Space: 7.4 MB (93.64%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -11,21 +11,20 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode *dummy = new ListNode(0); // created dummy node
-        dummy->next = head;
-        ListNode *prev = dummy; // intialising prev pointer on dummy node
-        
-        for(int i = 0; i < left - 1; i++)
-            prev = prev->next; // adjusting the prev pointer on it's actual index
-        
-        ListNode *curr = prev->next; // curr pointer will be just after prev
-        // reversing
-        for(int i = 0; i < right - left; i++){
-            ListNode *forw = curr->next; // forw pointer will be after curr
-            curr->next = forw->next;
-            forw->next = prev->next;
-            prev->next = forw;
+        if(left==right){return head;}
+        ListNode *temp=new ListNode(0);
+        ListNode *prev=temp;
+        prev->next=head;
+        for(int i=0;i<left-1;i++){
+            prev=prev->next;
         }
-        return dummy->next;
+        ListNode *cur=prev->next;
+        for(int i=0;i<right-left;i++){
+            ListNode *forw=cur->next;
+            cur->next=forw->next;
+            forw->next=prev->next;
+            prev->next=forw;
+        }
+        return temp->next;
     }
 };
