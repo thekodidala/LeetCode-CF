b904ecf90fd20a5368e4dad08accb867d94dc35b
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-09-10 20:09:39 +0530

    Time: 26 ms (35.18%), Space: 10.8 MB (31.47%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,18 +4,17 @@ public:
         int n=s.length();
         if(n==0)return 0;
         unordered_set<char>st;
-        int left=0,right=0;
+        int left=0;
         int maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            if(st.find(s[i])!=st.end()){
-                while(left<right&&st.find(s[i])!=st.end()){
+        for(int r=0;r<n;r++){
+            if(st.find(s[r])!=st.end()){
+                while(left<r&&st.find(s[r])!=st.end()){
                     st.erase(s[left]);
                     left++;
                 }
             }
-            st.insert(s[i]);
-            right++;
-            maxi=max(maxi,right-left);
+            st.insert(s[r]);
+            maxi=max(maxi,r-left+1);
         }
         return maxi;
     }
