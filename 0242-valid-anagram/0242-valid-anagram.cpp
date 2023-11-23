45275f39ad56fde1f689274d2526d8ab02749e57
Kodidala Vamshivardhan <95361527+thekodidala@users.noreply.github.com>
2023-11-23 22:02:47 +0530

    Time: 4 ms (86.85%), Space: 7.7 MB (78.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //int v[26]={0};
         vector<int>v(26,0);
-        if(s.length()!=t.length()){return false;}
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            v[s[i]-'a']++;
-            v[t[i]-'a']--;
+        for(int i=0;i<s.size();i++){
+            v[s[i]-'a']=v[s[i]-'a']+1;
         }
-        for(int i=0;i<26;i++){
-            if(v[i]){return false;}
+        for(int i=0;i<t.size();i++){
+            v[t[i]-'a']=v[t[i]-'a']-1;
+        }
+        for(int j=0;j<26;j++){
+            if(v[j]!=0){
+                return false;
+            }
+            cout<<v[j]<<" ";
         }
         return true;
     }
